ff986ccd19e05c7367e055ee24d675ba065e2702
Vlada992 <vladanng@gmail.com>
2020-04-17 14:12:10 +0200

    first commit

--- app.js
+++ app.js
@@ -21,7 +21,13 @@ app.post('/scrape', function(req, res){
 
     //make a new request to the URL provided in the HTTP POST request
     request(req.body.url, function (error, response, responseHtml) {
+        console.log(`response is ${response}`)
         var resObj = {};
+        var toggleClass = '.grey'
+
+
+        var array_1 = []
+        var arr_s = [".grey_bar", ".light_bar"]
 
         //if there was an error
         if (error) {
@@ -33,6 +39,39 @@ app.post('/scrape', function(req, res){
         resObj = {},
             //set a reference to the document that came back
             $ = cheerio.load(responseHtml),
+            
+            //console.log('this is', typeof $('.light_bar > td > a'))
+            //console.log(`to je bre ovo ${ $(`${toggleClass}_bar > td > a`).length }`)
+            
+
+              arr_s.map(function(i, elem) { 
+                //console.log(i, elem)
+                //console.log('so each of two class:', Object.entries( $(`${i} > td > a`)) )
+                array_1.push(Object.entries( $(`${i} > td > a`)))
+
+                Object.entries( $(`${i} > td > a`)).map((el, ind) => {
+                
+                    console.log("sta je ovo", el[1].attribs)
+                    
+                })
+               // array_1[i] = {
+                   //  a: $(this).find("td").find('a')
+                // };
+              });
+
+             // console.log(array_1.length)
+              //console.log('array1', array_1)
+
+            var storeResult1 = Object.entries( $(`.grey_bar > td > a`)      )  
+            storeResult1.map((el, ind) => {
+                
+                //console.log("sta je ovo", el[1].attribs)
+                //$Format = el[1].attribs.title.slice(0, el[1].attribs.title.indexOf('d'))
+                //$HrefLink = el[1].attribs.href
+            })
+
+            //console.log("ENTRIES OBJECTS ARE:", Object.entries(   $('.light_bar > td > a')  )[0]   )
+
             //create a reference to the meta elements
             $title = $('head title').text(),
             $desc = $('meta[name="description"]').attr('content'),
@@ -41,6 +80,14 @@ app.post('/scrape', function(req, res){
             $ogImage = $('meta[property="og:image"]').attr('content'),
             $ogkeywords = $('meta[property="og:keywords"]').attr('content'),
             $images = $('img');
+
+
+           // $format = 
+            //$FileFormatName: 
+
+        if ($Format) {
+            resObj.format = $Format;
+        }
 
         if ($title) {
             resObj.title = $title;
